b2e7e720aefd0c24dcbf320305dda00873ce09d1
Dmytro Brazhnyk <dmitry@brazhnik.org>
2018-08-27 12:00:28 +0300

    HashMap utils - Duplicated keys case

--- src/plugin/helpers/collections.rs
+++ src/plugin/helpers/collections.rs
@@ -1,21 +1,25 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+
 use std::hash::Hash;
 use std::fmt::Display;
 use std::any::Any;
 
-fn format_key_not_found<K>(k: &K) -> String{
-    format!("Key not found")
-}
-
 pub fn get_hashmap_values<'a, K: Eq + Hash, V>(map: &'a mut HashMap<K, V>, keys: &Vec<K>) -> Result<Vec<&'a V>, String>
 {
     let mut result = Vec::new();
+    let mut duplicated_key_set = HashSet::new();
 
     for key in keys {
+        if duplicated_key_set.contains(key) {
+            return Result::Err("Duplicated keys found".to_string());
+        }
+
+        duplicated_key_set.insert(key);
+
         let value_option = map.get(key);
 
         if value_option.is_none() {
-            return Result::Err(format_key_not_found(key));
+            return Result::Err("Key not found".to_string());
         }
 
         result.push(value_option.unwrap());
