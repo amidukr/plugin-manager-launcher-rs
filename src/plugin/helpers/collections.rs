b4a3b3bed7d21033b124544ff7c873c9884d373a
Dmytro Brazhnyk <dmitry@brazhnik.org>
2018-08-27 12:14:05 +0300

    HashMap utils - multi mut case

--- src/plugin/helpers/collections.rs
+++ src/plugin/helpers/collections.rs
@@ -4,25 +4,35 @@ use std::hash::Hash;
 use std::fmt::Display;
 use std::any::Any;
 
-pub fn get_hashmap_values<'a, K: Eq + Hash, V>(map: &'a mut HashMap<K, V>, keys: &Vec<K>) -> Result<Vec<&'a V>, String>
+ use std::mem::transmute;
+
+pub fn get_hashmap_values<'a, K: Eq + Hash, V>(map: &'a mut HashMap<K, V>, keys: &Vec<K>) -> Result<Vec<&'a mut V>, String>
 {
     let mut result = Vec::new();
     let mut duplicated_key_set = HashSet::new();
 
+    let ptr_map = map as *mut HashMap<K,V>;
+
     for key in keys {
-        if duplicated_key_set.contains(key) {
-            return Result::Err("Duplicated keys found".to_string());
+        unsafe {
+            
+            if duplicated_key_set.contains(key) {
+                return Result::Err("Duplicated keys found".to_string());
+            }
+
+            duplicated_key_set.insert(key);
+        
+        
+            let second_hash_map: &'a mut HashMap<K, V> = transmute(ptr_map);
+            let value_option = second_hash_map.get_mut(key);
+        
+
+            if value_option.is_none() {
+                return Result::Err("Key not found".to_string());
+            }
+
+            result.push(value_option.unwrap());
         }
-
-        duplicated_key_set.insert(key);
-
-        let value_option = map.get(key);
-
-        if value_option.is_none() {
-            return Result::Err("Key not found".to_string());
-        }
-
-        result.push(value_option.unwrap());
     }
 
     return Result::Ok(result);
